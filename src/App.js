6af39a1c9a5fa9316a4e415021c7c95cac1b2fe7
Farmanhud <52924457+Farmanhud@users.noreply.github.com>
2022-09-20 09:38:52 +0500

    museum-react almost

--- src/App.js
+++ src/App.js
@@ -1,62 +1,35 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import React from 'react'
+import "./App.css"
+import newsImage from "./images/cat.jpg"
 
-const URL = "https://type.fit/api/quotes";
 function App() {
-  const [data, setData] = useState([]);
-  const [oneItem, setOneItem] = useState([]);
-  const [index, setIndex] = useState(1640);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const res = await fetch(URL);
-        const data = await res.json();
-        setData(data);
-        console.log(data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getData();
-  }, []);
-
-  useEffect(() => {
-    if (data.length) {
-      setOneItem([data[index]]);
-    }
-  }, [data.length, index]);
-
-  const next = () => {
-    if (index < data.length - 1) {
-      return setIndex((index) => index + 1);
-    } else if (index === data.length - 1) {
-      return setIndex(0);
-    }
-  };
-
   return (
-    <div className="App" id="quote-box">
-      {oneItem?.map((item, index) => (
-        <div className="quote" key={index}>
-          <p className="text" id="text">
-            {item.text}
-          </p>
-          <p className="author" id="author=">
-            {item.author}
-          </p>
+    <div className='App'>
+      <div className="container">
+        <div className="title">
+        Respublika bo'yicha
+           <h2>  yangiliklar</h2>
         </div>
-      ))}
-      <div className="buttons">
-        <a href="twitter.com/intent/tweet" id="tweet-quote" target="_blank">
-          tweet
-        </a>
-        <button onClick={next} className="btn" id="new-quote">
-          New quote
-        </button>
+        <div className="main-news">
+          <div className="images">
+          <img src={newsImage} className="news-image"/>
+          </div>
+          
+          <div className="news-description">
+            <div className="news-description-info">
+              <div className="info">
+              <h4 className="news-title">Muzey Yangiliklari</h4>
+              <h4 className="news-location">Amaliy san'at muzeyi</h4>
+              <h4 className="news-updated-time">day(week): time</h4> 
+                </div>              
+                <p className='description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur voluptate ullam sed tempore quisquam doloribus rem excepturi ipsam neque recusandae culpa incidunt cumque perspiciatis perferendis et nihil cupiditate temporibus in nesciunt quam, ad voluptas facere beatae a! Voluptates, accusantium reiciendis.</p>
+            </div>
+            <div className="news-data">day month</div>
+          </div>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
